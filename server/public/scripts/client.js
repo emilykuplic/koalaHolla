8bd99b7456c77b18d62c8203d571bc5719f79e70
Emily Kuplic <emilykuplic@gmail.com>
2017-07-07 15:24:29 -0500

    koalas added to DOMnpm startnpm startnpm start

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -17,12 +17,6 @@ $( document ).ready( function(){
       sendKoala.gender = $('#genderIn').val();
       sendKoala.ready_for_transfer = $('#readyForTransferIn').val();
       sendKoala.notes = $('#notesIn').val();
-      // name: 'testName',
-      // age: 'testName',
-      // gender: 'testName',
-      // readyForTransfer: 'testName',
-      // notes: 'testName',
-
 
     // call saveKoala with the new obejct
     saveKoala( sendKoala );
@@ -35,8 +29,11 @@ function getKoalas(){
   $.ajax({
     url: '/koalas',
     type: 'GET',
-    success: function( data ){
+    //sending back an object with an array of objects (koalas)
+    success: function(data){
       console.log( 'got some koalas: ', data );
+      var koalas = data.koalas; // Array of koalas
+      appendToDom(koalas);
     } // end success
   }); //end ajax
   // display on DOM with buttons that allow edit of each
@@ -53,4 +50,21 @@ function saveKoala( newKoala ){
       console.log( 'got some koalas: ', data );
     } // end success
   }); //end ajax
+}
+// Append array of books to the DOM
+function appendToDom(koalas) {
+  // Remove books that currently exist in the table
+  $('#viewKoalas').empty();
+  for(var i = 0; i < koalas.length; i += 1) {
+    var koalaDOM = koalas[i];
+    // For each book, append a new row to our table
+    $tr = $('<tr></tr>');
+    $tr.data('koalas', koalaDOM);
+    $tr.append('<td>' + koalaDOM.koala_name + '</td>');
+    $tr.append('<td>' + koalaDOM.age + '</td>');
+    $tr.append('<td>' + koalaDOM.gender + '</td>');
+    $tr.append('<td>' + koalaDOM.ready_for_transfer + '</td>');
+    $tr.append('<td>' + koalaDOM.notes + '</td>');
+    $('#viewKoalas').append($tr);
+}
 }
